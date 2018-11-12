9d89cd38b28c2a3e77012496b0a5bf9872831b51
avalon <yuanqi.shan@aalto.fi>
2018-11-12 15:55:31 +0900

    Import nature kit, create temp forset scene

---
+++ Assets/_Scripts/Game/Draggable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour {
+
+    protected Transform originalParent;
+
+    private void Awake()
+    {
+        originalParent = transform.parent;
+    }
+
+    public virtual void OnDrag(Transform parent)
+    {
+        transform.SetParent(parent);
+    }
+
+    public virtual void OnRelease(Transform parent)
+    {
+        transform.SetParent(originalParent);
+    }
+}
