b6f324a13bc39b781267a18b963992c25026612b
avalon <yuanqi.shan@aalto.fi>
2019-02-13 08:34:09 +0900

    Bump sound

--- Assets/_Scripts/Game/World/Combinable.cs
+++ Assets/_Scripts/Game/World/Combinable.cs
@@ -7,6 +7,8 @@ public class Combinable : MonoBehaviour
     public string[] linesMove = new string[] { "Unable to move.\n (Combined with an immovable object).",
     "Press TAB to revert dimensions." };
 
+    private static readonly string bumpSfxName = "Bump";
+
     public virtual void HandleUpdate(Player2D player)
     {
         if(linesMove.Length > 0)
@@ -14,9 +16,14 @@ public class Combinable : MonoBehaviour
             var moveDelta = new Vector2(Input.GetAxis(Static.horizontalAxis), Input.GetAxis(Static.verticalAxis));
 
             if (moveDelta.x != 0 || moveDelta.y != 0)
+            {
                 TransitionScreen.Instance.dialogueWindow.SetDialogue(linesMove);
+                AudioManager.Instance.PlaySFX(bumpSfxName);
+            }
             else
+            {
                 player.CheckTransition(true);
+            }
             return;
         }
         player.Locomotion();
