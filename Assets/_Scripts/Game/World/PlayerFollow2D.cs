9a36aabfe456a69d51443e4014e8f6f4b450e09e
avalon <yuanqi.shan@aalto.fi>
2019-02-13 09:46:48 +0900

    Parallex background

---
+++ Assets/_Scripts/Game/World/PlayerFollow2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow2D : MonoBehaviour
+{
+    public float multiplier = 1.0f;
+    private Vector3 oldPosition;
+
+    private void Start()
+    {
+        oldPosition = GameManager.Instance.player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = GameManager.Instance.player.transform.position;
+        Vector3 delta = newPosition - oldPosition;
+        transform.position += delta * multiplier;
+        oldPosition = newPosition;
+    }
+}
