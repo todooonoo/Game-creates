b4a3cd78f275d9868358de3b984c5fe623689399
avalon <yuanqi.shan@aalto.fi>
2019-02-18 00:44:03 +0900

    Laser sound

---
+++ Assets/_Scripts/Game/ForceOnContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceOnContact : MonoBehaviour
+{
+    public Vector3 force;
+    private static string bumpSFXName = "Bump";
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rBody = collision.collider.attachedRigidbody;
+
+        if(rBody)
+        {
+            rBody.AddForce(force, ForceMode.VelocityChange);
+            AudioManager.Instance.PlaySFX(bumpSFXName);
+        }
+    }
+}
